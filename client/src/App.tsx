2f7398bc759124002d09bffe1bc1faf1e6d27301
Alex Soong <alexsoong91@gmail.com>
2023-07-11 13:57:57 -0700

    Introduce basic bottom nav component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,24 +5,24 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
-  Link,
   Navigate,
 } from "react-router-dom";
 import { useLocation } from "react-use";
-import styled from "styled-components";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { BottomNav } from "./components/layouts/BottomNav";
+
 
 const App = () => {
   return (
     <Router>
       <div>
-        <NavSection />
 
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/" element={<Navigate to={"/"} replace={true} />} />
           <Route element={<>Not found</>} />
         </Routes>
+
+        <BottomNav />
       </div>
     </Router>
   );
@@ -35,39 +35,3 @@ const Main: React.FC = () => {
 
   return <MainPage key={search} />;
 };
-
-const NavSection: React.FC = () => {
-  const { pathname } = useLocation();
-
-  return (
-    <Nav>
-      <Logo className={pathname === "/" ? "current_page" : "off"} to={"/"}>
-        ZK-P2P-On-Ramp
-      </Logo>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          gap: "1rem",
-        }}
-      >
-        <ConnectButton />
-      </div>
-    </Nav>
-  );
-};
-
-const Logo = styled(Link)`
-  text-transform: uppercase;
-  letter-spacing: 0.04em;
-  color: #fff;
-  text-decoration: none;
-  font-size: 1.2rem;
-`;
-
-const Nav = styled.nav`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin: 12px;
-`;
