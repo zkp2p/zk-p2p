b73281a24e7916219d352ce308e4eda4cdaf03b5
Richard Liang <richardliang2015@u.northwestern.edu>
2023-07-26 22:58:55 -0700

    Add new script (#12)

---
+++ circuits-circom/scripts/3_gen_zkey_unsafe.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Tries to generate a chunked and non-chunked zkey
+# You need to set entropy.env for this to work
+
+source circuit.env
+
+R1CS_FILE="$BUILD_DIR/$CIRCUIT_NAME.r1cs"
+PARTIAL_ZKEYS="$BUILD_DIR"/partial_zkeys
+PHASE1=../powersOfTau28_hez_final_$PTAU.ptau
+source entropy.env
+
+if [ ! -d "$BUILD_DIR"/partial_zkeys ]; then
+    echo "No partial_zkeys directory found. Creating partial_zkeys directory..."
+    mkdir -p "$BUILD_DIR"/partial_zkeys
+fi
+
+# Then, nonchunked snarkjs
+yarn remove snarkjs
+# mv ../yarn.lock ../yarn.lock_old2
+# rm -rf ../node_modules_old2
+# mv ../node_modules ../node_modules_old2
+yarn add snarkjs@latest
+
+echo "****GENERATING ZKEY NONCHUNKED FINAL****"
+start=$(date +%s)
+set -x
+NODE_OPTIONS='--max-old-space-size=56000' node ../node_modules/.bin/snarkjs zkey new "$CIRCUIT_NAME".r1cs "$PHASE1" "$BUILD_DIR"/"$CIRCUIT_NAME".zkey -v
+{ set +x; } 2>/dev/null
+end=$(date +%s)
+echo "DONE ($((end - start))s)"
+echo
+
+yarn remove snarkjs
+# mv ../yarn.lock ../yarn.lock_old3
+# rm -rf ../node_modules_old3
+# mv ../node_modules ../node_modules_old3
+yarn add snarkjs@git+https://github.com/vb7401/snarkjs.git#24981febe8826b6ab76ae4d76cf7f9142919d2b8
+yarn
