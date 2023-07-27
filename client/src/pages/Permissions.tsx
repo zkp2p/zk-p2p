f98cea23dd56d831c43ef525b6853eff8aef107c
Alex Soong <alexsoong91@gmail.com>
2023-07-27 01:58:35 -0700

    Remove unused PageWrapper styles

--- client/src/pages/Permissions.tsx
+++ client/src/pages/Permissions.tsx
@@ -17,23 +17,6 @@ const PageWrapper = styled.div`
   display: flex;
   flex-direction: column;
   padding: 8px 8px 0px;
-
-  & .title {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  
-  & .main {
-    & .signaturePane {
-      flex: 1;
-      display: flex;
-      flex-direction: column;
-      & > :first-child {
-        height: calc(30vh + 24px);
-      }
-    }
-  }
 `;
 
 const Main = styled.div`
