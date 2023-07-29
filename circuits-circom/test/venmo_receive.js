527b804536d5eec139599b2cb58cab307f56c226
Richard Liang <richardliang2015@u.northwestern.edu>
2023-07-28 17:14:01 -0700

    Save WIP

--- circuits-circom/test/venmo_receive.js
+++ circuits-circom/test/venmo_receive.js
@@ -38,6 +38,8 @@ describe("Venmo receive test", function () {
 
     let cir;
 
+    const PUBLIC_INPUT_LEN = 32;
+
     before( async() => {
         cir = await wasm_tester(
             path.join(__dirname, "../venmo_receive.circom"),
@@ -71,6 +73,36 @@ describe("Venmo receive test", function () {
             input,
             true
         );
+
+        assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)));
+    }).timeout(1000000);
+
+    it.only("Should return the correct packed timestamp", async () => {
+        // To preserve privacy of emails, load inputs generated using `yarn gen-input`. Ping us if you want an example venmo_receive.eml to run tests 
+        // Otherwise, you can download the original eml from any Venmo receive payment transaction
+        const venmo_path = path.join(__dirname, "../inputs/input_venmo_receive.json");
+        const jsonString = fs.readFileSync(venmo_path, "utf8");
+        const input = JSON.parse(jsonString);
+        const witness = await cir.calculateWitness(
+            input,
+            true
+        );
+
+        // Get returned packed timestamp
+        // Indexes 1 to 5 inclusive represent the packed timestamp
+        const packed_timestamp = witness.slice(1, 6);
+        console.log(packed_timestamp)
+
+        // Get expected packed timestamp
+        const regex_start = Number(input["email_timestamp_idx"]);
+        const regex_start_sub_array = input["in_padded"].slice(regex_start);
+        const regex_end = regex_start_sub_array.indexOf("59"); // Look for `;` to end the timestamp which is 59 in ascii
+        const timestamp_array = regex_start_sub_array.slice(0, regex_end);
+        console.log(timestamp_array)
+
+        // Pack bytes into 7 byte representation
+        
+        
 
         assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)));
         // TODO: Add more tests
