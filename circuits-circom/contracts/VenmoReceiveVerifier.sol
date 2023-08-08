5d4b1665e01b15e384736161932c253f961e8bb3
Richard Liang <richardliang2015@u.northwestern.edu>
2023-08-08 10:20:29 -0700

    Rename verifiers (#24)

--- circuits-circom/contracts/VenmoReceiveVerifier.sol
+++ circuits-circom/contracts/VenmoReceiveVerifier.sol
@@ -161,7 +161,7 @@ library Pairing {
         return pairing(p1, p2);
     }
 }
-contract Verifier {
+contract VenmoReceiveVerifier {
     using Pairing for *;
     struct VerifyingKey {
         Pairing.G1Point alfa1;
