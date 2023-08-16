786baf889653be8264f65c479b4cdba8506314bd
Alex Soong <alexsoong91@gmail.com>
2023-08-16 13:01:16 -0700

    Add Hardhat Configuratioj (#33)
    * Update ramp contract abi
    
    * Add fakeUSDC abi
    
    * Enable hardhat in configs and update README.md
    
    * Add hardhat entry into deployed addresses config

--- client/src/helpers/deployed_addresses.ts
+++ client/src/helpers/deployed_addresses.ts
@@ -5,17 +5,16 @@ type Contracts = {
 };
 
 export const contractAddresses: Contracts = {
-  "goerli": {
-    "ramp": '0xE62e588756576ECE4a8dCAEBA94dd2889dc6A04c',
-    "fusdc": '0xf6426A1fdE02c3d6f10b4af107cDd7669574E74C',
+  "hardhat": {
+    "ramp": '0xdc64a140aa3e981100a9beca4e685f962f0cf6c9',
+    "fusdc": '0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9',
   },
-  // Legacy
-  // "goerli": {
-  //   "ramp": '0x945D14a5c63769f4cf008a2994810940cc0DFd5C',
-  //   "fusdc": '0xf6426A1fdE02c3d6f10b4af107cDd7669574E74C',
-  // },
-  "mantle": {
-    "ramp": '0xa8B1e2A1235E2B30B32233b9176867A05B48dc3e',
-    "fusdc": '0xA6E289496d6479a5c7cAD48745990ef534FD3314',
+  "goerli": {
+    "ramp": '',
+    "fusdc": '',
+  },
+  "optimism": {
+    "ramp": '',
+    "fusdc": '',
   },
 };
