386c3626245bb3b93038782f6ed3d24aad50f32c
Alex Soong <alexsoong91@gmail.com>
2023-08-27 23:02:41 -0700

    Add smart contract structs to types and refetch from RampRegistrationProvider

--- client/src/contexts/RampRegistration/RampRegistrationProvider.tsx
+++ client/src/contexts/RampRegistration/RampRegistrationProvider.tsx
@@ -3,7 +3,7 @@ import { useContractRead } from 'wagmi'
 
 import useAccount from '../../hooks/useAccount'
 import RampRegistrationContext from './RampRegistrationContext'
-import AccountInfo from './types'
+import { Deposit } from './types'
 import { abi } from "../../helpers/abi/ramp.abi";
 
 
@@ -21,63 +21,127 @@ const AccountProvider = ({ children }: ProvidersProps) => {
    * State
    */
   const [registrationHash, setRegistrationHash] = useState<string>("");
+  const [deposits, setDeposits] = useState<Deposit[]>([]);
 
   /*
-   * Contract Reads
+   * Contract Reads (migrate to: https://wagmi.sh/react/hooks/useContractReads)
    */
 
-  // mapping(address => AccountInfo) public accounts;
+  // function getAccountVenmoId(address _account) external view returns (bytes32) {
   const {
-    data: registrationDataRaw,
-    // isLoading: isRegistrationDataLoading,
+    data: venmoIdHashRaw,
+    // isLoading: isFetchVenmoIdHashLoading,
     // isError: isRegistrationDataError,
-    // refetch: refetchRegistrationData,
+    // refetch: refetchVenmoIdHash,
   } = useContractRead({
     address: rampAddress as `0x${string}`,
     abi: abi,
-    functionName: 'accounts',
+    functionName: 'getAccountVenmoId',
     args: [ethereumAddress],
   })
+
+  // function getAccountDeposits(address _account) external view returns (Deposit[] memory accountDeposits) {
+  const {
+    data: depositsRaw,
+    isLoading: isFetchDepositsLoading,
+    isError: isFetchDepositsError,
+    // refetch: refetchDeposits,
+  } = useContractRead({
+    address: rampAddress as `0x${string}`,
+    abi: abi,
+    functionName: 'getAccountDeposits',
+    args: [ethereumAddress],
+  })
+
+  // mapping(bytes32 => Intent) public intents;
+  // const {
+  //   data: intentRaw,
+  //   // isLoading: isFetchVenmoIdHashLoading,
+  //   // isError: isRegistrationDataError,
+  //   // refetch: refetchVenmoIdHash,
+  // } = useContractRead({
+  //   address: rampAddress as `0x${string}`,
+  //   abi: abi,
+  //   functionName: 'intents',
+  //   args: [registrationHash],
+  // })
+
+  /*
+   * Additional Reads:
+   */
+
+  // mapping(address => AccountInfo) public accounts;
+  // mapping(bytes32 => bytes32) public venmoIdIntent;
+
+  // function getDeposit(uint256 _depositId) external view returns (Deposit memory) {
+  // function getDepositFromIds(uint256[] memory _depositIds) external view returns (Deposit[] memory depositArray) {
 
   /*
    * Hooks
    */
   useEffect(() => {
-    console.log('registrationDataRaw_1');
-    console.log(registrationDataRaw);
+    console.log('venmoIdHash_1');
+    console.log(venmoIdHashRaw);
 
-    if (registrationDataRaw) {
-      const registrationData = registrationDataRaw as AccountInfo;
-      
-      console.log('registrationDataRaw_2');
-      console.log(registrationData);
+    if (venmoIdHashRaw) {
+      console.log('venmoIdHash_2');
+      console.log(venmoIdHashRaw);
   
-      if (ethereumAddress && registrationData.venmoIdHash) {
-        setRegistrationHash(registrationData.venmoIdHash);
+      if (ethereumAddress && venmoIdHashRaw) {
+        setRegistrationHash(venmoIdHashRaw as string);
       } else {
         setRegistrationHash("");
       }
     }
-  }, [ethereumAddress, registrationDataRaw]);
+  }, [ethereumAddress, venmoIdHashRaw]);
 
   // useEffect(() => {
-  //   console.log('refetchRegistrationData');
+  //   console.log('refetchVenmoIdHash');
 
   //   if (ethereumAddress) {
   //     const intervalId = setInterval(() => {
-  //       refetchRegistrationData();
+  //       refetchVenmoIdHash();
   //     }, 15000); // Refetch every 15 seconds
   
   //     return () => {
   //       clearInterval(intervalId);
   //     };
   //   }
-  // }, [ethereumAddress, refetchRegistrationData]);
+  // }, [ethereumAddress, refetchVenmoIdHash]);
+
+  useEffect(() => {
+    if (!isFetchDepositsLoading && !isFetchDepositsError && depositsRaw) {
+      const depositsArrayRaw = depositsRaw as any[];
+
+      const sanitizedDeposits: Deposit[] = [];
+      for (let i = depositsArrayRaw.length - 1; i >= 0; i--) {
+        const depositData = depositsArrayRaw[i];
+        
+        const deposit: Deposit = {
+          depositor: depositData.depositor.toString(),
+          remainingDepositAmount: depositData.remainingDeposits.toString(),
+          outstandingIntentAmount: depositData.outstandingIntentAmount.toString(),
+          conversionRate: depositData.conversionRate.toString(),
+          convenienceFee: depositData.convenienceFee.toString(),
+        };
+
+        sanitizedDeposits.push(deposit);
+      }
+
+      if (ethereumAddress && depositsRaw) {
+        setDeposits(sanitizedDeposits);
+        console.log(sanitizedDeposits);
+      } else {
+        setDeposits([]);
+      }
+    }
+  }, [ethereumAddress, depositsRaw, isFetchDepositsLoading, isFetchDepositsError]);
 
   return (
     <RampRegistrationContext.Provider
       value={{
-        registrationHash
+        registrationHash,
+        deposits,
       }}
     >
       {children}
--- client/src/contexts/RampRegistration/types.ts
+++ client/src/contexts/RampRegistration/types.ts
@@ -1,6 +1,37 @@
-import { Deposit } from '../../helpers/types';
-
-export default interface AccountInfo {
+// struct AccountInfo {
+//   bytes32 venmoIdHash;
+//   uint256[] deposits;
+// }
+export interface AccountInfo {
   venmoIdHash: string;
   deposits: Deposit[];
+}
+
+// struct Deposit {
+//   address depositor;
+//   uint256 remainingDeposits;          // Amount of remaining deposited liquidity
+//   uint256 outstandingIntentAmount;    // Amount of outstanding intents (may include expired intents)
+//   uint256 conversionRate;             // Conversion required by off-ramper between USDC/USD
+//   uint256 convenienceFee;             // Amount of USDC per on-ramp transaction available to be claimed by off-ramper
+//   bytes32[] intentHashes;             // Array of hashes of all open intents (may include some expired if not pruned)
+// }
+export interface Deposit {
+  depositor: string;
+  remainingDepositAmount: number;
+  outstandingIntentAmount: number;
+  conversionRate: number;
+  convenienceFee: number;
+}
+
+// struct Intent {
+//   address onramper;
+//   uint256 deposit;
+//   uint256 amount;
+//   uint256 intentTimestamp;
+// }
+export interface Intent {
+  onRamper: string;
+  deposit: string;
+  amount: number;
+  timestamp: number;
 }
