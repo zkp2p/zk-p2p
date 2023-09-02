58da2d89ddc86f1b0dd265548704d03af2b0379c
Richard Liang <richardliang2015@u.northwestern.edu>
2023-09-01 23:16:31 -0700

    Update tests

--- circuits-circom/test/venmo_registration.js
+++ circuits-circom/test/venmo_registration.js
@@ -1,63 +1,17 @@
 const chai = require("chai");
 const path = require("path");
-const crypto = require("crypto");
 const F1Field = require("ffjavascript").F1Field;
 const Scalar = require("ffjavascript").Scalar;
 exports.p = Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495617");
 const Fr = new F1Field(exports.p);
 const buildPoseidon = require("circomlibjs").buildPoseidonOpt;
+const { chunkArray, bytesToPacked } = require("./utils.js");
 
 const assert = chai.assert;
 
 const wasm_tester = require("circom_tester").wasm;
 
 const fs = require('fs');
-
-
-// TODO: move helpers into utils, update to use TS
-function buffer2bitArray(b) {
-    const res = [];
-    for (let i = 0; i < b.length; i++) {
-        for (let j = 0; j < 8; j++) {
-            res.push((b[i] >> (7 - j) & 1));
-        }
-    }
-    return res;
-}
-
-function bitArray2buffer(a) {
-    const len = Math.floor((a.length - 1) / 8) + 1;
-    const b = new Buffer.alloc(len);
-
-    for (let i = 0; i < a.length; i++) {
-        const p = Math.floor(i / 8);
-        b[p] = b[p] | (Number(a[i]) << (7 - (i % 8)));
-    }
-    return b;
-}
-
-function bytesToPacked(arr) {
-    // Convert into bigint from string
-    let arrInt = arr.map(BigInt);
-    let n = arrInt.length;
-    let out = BigInt(0);
-    for (let k = 0; k < n; k++) {
-        out += arrInt[k] * BigInt(2 ** (8 * k));  // little endian
-    }
-    return out;
-}
-
-function chunkArray(arr, chunkSize, length) {
-    let chunks = [];
-    for (let i = 0; i < length; i += chunkSize) {
-        let chunk = arr.slice(i, i + chunkSize);
-        if (chunk.length < chunkSize) {
-            chunk = chunk.concat(new Array(chunkSize - chunk.length).fill('0'));
-        }
-        chunks.push(chunk);
-    }
-    return chunks;
-}
 
 describe("Venmo Registration", function () {
     this.timeout(100000);
