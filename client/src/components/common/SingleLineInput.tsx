575a6a3ea5a628d527ca04ec0d0b3562478ffb4e
Alex Soong <alexsoong91@gmail.com>
2023-09-06 18:09:41 -0700

    Update SingleLineInput to be able to accept an error string

--- client/src/components/common/SingleLineInput.tsx
+++ client/src/components/common/SingleLineInput.tsx
@@ -8,7 +8,15 @@ export const SingleLineInput: React.FC<{
   placeholder: string;
   onChange: (e: any) => void;
   readOnly?: boolean;
-}> = ({ label, value, placeholder, onChange, readOnly = false }) => {
+  error?: string | null;
+}> = ({
+  label,
+  value,
+  placeholder,
+  onChange,
+  readOnly = false,
+  error = null,
+}) => {
   return (
     <InputContainer>
       <label
@@ -23,7 +31,13 @@ export const SingleLineInput: React.FC<{
         value={value}
         placeholder={placeholder}
         readOnly={readOnly}
+        hasError={!!error}
       />
+      { error &&
+        <ErrorMessage>
+          {error}
+        </ErrorMessage>
+      }
     </InputContainer>
   );
 };
@@ -32,8 +46,8 @@ const InputContainer = styled(Col)`
   gap: 8px;
 `;
 
-const Input = styled.input`
-  border: 1px solid rgba(255, 255, 255, 0.4);
+const Input = styled.input<{ hasError: boolean }>`
+  border: 1px solid ${props => props.hasError ? 'red' : 'rgba(255, 255, 255, 0.4)'};
   background: rgba(0, 0, 0, 0.3);
   border-radius: 4px;
   padding: 8px 12px;
@@ -43,7 +57,14 @@ const Input = styled.input`
   color: #fff;
   font-size: 16px;
   transition: all 0.2s ease-in-out;
+
   &:hover {
     border: 1px solid rgba(255, 255, 255, 0.8);
   }
+`;
+
+const ErrorMessage = styled.div`
+  color: red;
+  font-size: 12px;
+  margin-top: 4px;
 `;
