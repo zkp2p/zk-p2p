dfcb36612a610ee131c2bb2ecb88f96306dc9fed
0xSachinK <sachin@setprotocol.com>
2023-09-14 13:30:48 +0530

    Add compile unsafe scripts

---
+++ circuits-circom/scripts/1_compile_unsafe.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source circuit.env
+
+if [ ! -d "$BUILD_DIR" ]; then
+    echo "No build directory found. Creating build directory..."
+    mkdir -p "$BUILD_DIR"
+fi
+
+echo '****COMPILING CIRCUIT****'
+start=$(date +%s)
+set -x
+circom "../$CIRCUIT_NAME".circom --r1cs --wasm --sym --c --wat --output "$BUILD_DIR" -l "../node_modules"
+{ set +x; } 2>/dev/null
+end=$(date +%s)
+echo "DONE ($((end - start))s)"
+echo
