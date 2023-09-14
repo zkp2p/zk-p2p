67d97e04d517f2f39cd74e227df4aa81bf18041d
0xSachinK <sachin@setprotocol.com>
2023-09-14 20:02:22 +0530

    fix bug

--- circuits-circom/scripts/7_upload_to_s3.sh
+++ circuits-circom/scripts/7_upload_to_s3.sh
@@ -4,7 +4,7 @@ source circuit.env
 echo $UPLOAD_FOLDER
 
 echo "Uploading partial keys to s3..."
-python3 upload_to_s3.py --dirs ../build/$CIRCUIT_NAME/partial_zkeys/ --upload_dir $UPLOAD_FOLDER
+python3 upload_to_s3.py --dirs ../build/$CIRCUIT_NAME/partial_zkeys/ --upload_dir $UPLOAD_FOLDER/$CIRCUIT_NAME
 
 echo "Uploading wasm to s3..."
-python3 upload_to_s3.py --dirs ../build/$CIRCUIT_NAME/$CIRCUIT_NAME_js/ --upload_dir $UPLOAD_FOLDER
+python3 upload_to_s3.py --dirs ../build/$CIRCUIT_NAME/$CIRCUIT_NAME_js/ --upload_dir $UPLOAD_FOLDER/$CIRCUIT_NAME
