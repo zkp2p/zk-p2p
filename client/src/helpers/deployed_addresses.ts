52c4ad96432aa8a763544ce8e85dee2c19edc1d9
Alex Soong <alexsoong91@gmail.com>
2023-09-14 21:46:49 -0700

    Update new deployed addresses

--- client/src/helpers/deployed_addresses.ts
+++ client/src/helpers/deployed_addresses.ts
@@ -6,7 +6,7 @@ type Contracts = {
 
 export const contractAddresses: Contracts = {
   "hardhat": {
-    "ramp": '0xdc64a140aa3e981100a9beca4e685f962f0cf6c9',
+    "ramp": '0x5fc8d32690cc91d4c39d9d3abcbd16989f875707',
     "fusdc": '0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9',
   },
   "goerli": {
