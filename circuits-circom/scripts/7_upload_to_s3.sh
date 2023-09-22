6698a44187101f44db1c1c9de988659da3c8cd2c
0xSachinK <sachin@setprotocol.com>
2023-09-22 11:57:06 +0100

    fix script

--- circuits-circom/scripts/7_upload_to_s3.sh
+++ circuits-circom/scripts/7_upload_to_s3.sh
@@ -10,4 +10,4 @@ echo $UPLOAD_FOLDER
 # python3 upload_to_s3.py --dirs ../build/$CIRCUIT_NAME/"$CIRCUIT_NAME"_js/ --upload_dir $UPLOAD_FOLDER/$CIRCUIT_NAME
 
 echo "Uploading veky.json to s3..."
-python3 upload_to_s3.py --dirs ../build/$CIRCUIT_NAME --upload_dir $UPLOAD_FOLDER/$CIRCUIT_NAME
+python3 upload_to_s3.py --dirs ../build/$CIRCUIT_NAME/ --upload_dir $UPLOAD_FOLDER/$CIRCUIT_NAME
