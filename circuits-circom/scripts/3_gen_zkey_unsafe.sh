cde4313bca5dd13028527fbdbe81c5cb882368c7
0xSachinK <sachin@setprotocol.com>
2023-09-22 11:52:08 +0100

    Add vkey gen and upload script

--- circuits-circom/scripts/3_gen_zkey_unsafe.sh
+++ circuits-circom/scripts/3_gen_zkey_unsafe.sh
@@ -21,14 +21,18 @@ yarn remove snarkjs
 # mv ../node_modules ../node_modules_old2
 yarn add snarkjs@latest
 
-echo "****GENERATING ZKEY NONCHUNKED FINAL****"
-start=$(date +%s)
-set -x
-NODE_OPTIONS='--max-old-space-size=56000' node ../node_modules/.bin/snarkjs zkey new "$BUILD_DIR"/"$CIRCUIT_NAME".r1cs "$PHASE1" "$BUILD_DIR"/"$CIRCUIT_NAME".zkey -v
-{ set +x; } 2>/dev/null
-end=$(date +%s)
-echo "DONE ($((end - start))s)"
-echo
+# echo "****GENERATING ZKEY NONCHUNKED FINAL****"
+# start=$(date +%s)
+# set -x
+# NODE_OPTIONS='--max-old-space-size=56000' node ../node_modules/.bin/snarkjs zkey new "$BUILD_DIR"/"$CIRCUIT_NAME".r1cs "$PHASE1" "$BUILD_DIR"/"$CIRCUIT_NAME".zkey -v
+# { set +x; } 2>/dev/null
+# end=$(date +%s)
+# echo "DONE ($((end - start))s)"
+# echo
+
+# Export the verification key to JSON
+echo "Exporting verification key to JSON..."
+node ../node_modules/.bin/snarkjs zkey export verificationkey "$BUILD_DIR"/"$CIRCUIT_NAME".zkey "$BUILD_DIR"/"$CIRCUIT_NAME"_vkey.json
 
 yarn remove snarkjs
 # mv ../yarn.lock ../yarn.lock_old3
--- circuits-circom/scripts/7_upload_to_s3.sh
+++ circuits-circom/scripts/7_upload_to_s3.sh
@@ -3,8 +3,11 @@
 source circuit.env
 echo $UPLOAD_FOLDER
 
-echo "Uploading partial keys to s3..."
-python3 upload_to_s3.py --dirs ../build/$CIRCUIT_NAME/partial_zkeys/ --upload_dir $UPLOAD_FOLDER/$CIRCUIT_NAME
+# echo "Uploading partial keys to s3..."
+# python3 upload_to_s3.py --dirs ../build/$CIRCUIT_NAME/partial_zkeys/ --upload_dir $UPLOAD_FOLDER/$CIRCUIT_NAME
 
-echo "Uploading wasm to s3..."
-python3 upload_to_s3.py --dirs ../build/$CIRCUIT_NAME/"$CIRCUIT_NAME"_js/ --upload_dir $UPLOAD_FOLDER/$CIRCUIT_NAME
+# echo "Uploading wasm to s3..."
+# python3 upload_to_s3.py --dirs ../build/$CIRCUIT_NAME/"$CIRCUIT_NAME"_js/ --upload_dir $UPLOAD_FOLDER/$CIRCUIT_NAME
+
+echo "Uploading veky.json to s3..."
+python3 upload_to_s3.py --dirs ../build/$CIRCUIT_NAME --upload_dir $UPLOAD_FOLDER/$CIRCUIT_NAME
