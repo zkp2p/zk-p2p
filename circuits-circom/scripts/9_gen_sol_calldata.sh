7f66a93f74a0c53d35210ee1287acd464305e809
0xSachinK <sachin@setprotocol.com>
2023-09-27 02:02:36 +0530

    fix script

--- circuits-circom/scripts/9_gen_sol_calldata.sh
+++ circuits-circom/scripts/9_gen_sol_calldata.sh
@@ -2,4 +2,4 @@
 source circuit.env
 
 echo "Generating solidity calldata..."
-node ../node_modules/.bin/snarkjs zkesc "$BUILD_DIR"/rapidsnark_proof.json "$BUILD_DIR"/rapidsnark_public.json
+node ../node_modules/.bin/snarkjs zkey export soliditycalldata "$BUILD_DIR"/rapidsnark_public.json "$BUILD_DIR"/rapidsnark_proof.json
