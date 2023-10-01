bedca7776cad75f2083f200df2e7b2499661e225
Alex Soong <alexsoong91@gmail.com>
2023-09-30 23:25:06 -0700

    Update packing on poseidon hash for 3 words

--- client/src/helpers/poseidonHash.ts
+++ client/src/helpers/poseidonHash.ts
@@ -33,11 +33,11 @@ export const calculateVenmoIdHash = async(venmoId: string): Promise<string> => {
   return BigNumber.from(poseidon.F.toString(poseidon(packedVenmoId))).toHexString();
 }
 
-export const calculatePackedVenmoId = (venmoId: string): [BigNumber, BigNumber, BigNumber, BigNumber, BigNumber] => {
+export const calculatePackedVenmoId = (venmoId: string): [BigNumber, BigNumber, BigNumber] => {
   const venmoIdArray: number[] = venmoId.split('').map(char => char.charCodeAt(0));
 
   // Pad with zeros until length is 30
-  while (venmoIdArray.length < 30) {
+  while (venmoIdArray.length < 21) {
     venmoIdArray.push(0);
   }
 
@@ -46,12 +46,10 @@ export const calculatePackedVenmoId = (venmoId: string): [BigNumber, BigNumber, 
 
 function Bytes2Packed(n: number, inArr: number[]) {
   let index = 0;
-  const out: [BigNumber, BigNumber, BigNumber, BigNumber, BigNumber] = [
+  const out: [BigNumber, BigNumber, BigNumber] = [
     constants.Zero,
     constants.Zero,
     constants.Zero,
-    constants.Zero,
-    constants.Zero
   ];
 
   for (let i = 0; i < inArr.length; i += n) {
