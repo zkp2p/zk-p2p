4f2e1c08a22bbdf92030e2b5cdac9641256da588
Alex Soong <alexsoong91@gmail.com>
2023-10-23 16:15:00 -0700

    Update deployed addresses for 0.0.8

--- client/src/helpers/deployed_addresses.ts
+++ client/src/helpers/deployed_addresses.ts
@@ -13,6 +13,13 @@ export const contractAddresses: Contracts = {
     "registrationProcessor": '0xdc64a140aa3e981100a9beca4e685f962f0cf6c9',
   },
   "goerli": {
+    "ramp": '0x1A2dcc7AfC7Bf09bE718E54466aA18166C00E4FD',
+    "fusdc": '0x0a5A6f847899e185a14e54266036CE8260142659',
+    "receiveProcessor": '0x8E96C1c53bD6Ec2f54D4788629c423ac35fA6d75',
+    "sendProcessor": '0x0a7537A73079C45f64623D213Cd284E9b55Fe0b7',
+    "registrationProcessor": '0x8D54b20922aEA549224D61501225a0DaB5bC3839',
+  },
+  "goerli_legacy": {
     "ramp": '0x75422735DD94dfD04b7ef5D7044Aba0ce4E3a7A6',
     "fusdc": '0xbba5c9bd54a4293f4261b38e5ad41820ec41ed86',
     "receiveProcessor": '0xF05627d52F3b4b173002Ee64E90FEc169db77057',
