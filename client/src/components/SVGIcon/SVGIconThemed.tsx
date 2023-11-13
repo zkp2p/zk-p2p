3fe9f242f5adbf1ab8287b808c5a3e684475794f
Justin Chen <justin@setprotocol.com>
2023-11-13 04:20:18 +0300

    Add back in icon css

--- client/src/components/SVGIcon/SVGIconThemed.tsx
+++ client/src/components/SVGIcon/SVGIconThemed.tsx
@@ -1,11 +1,12 @@
 import React, { CSSProperties } from 'react';
 
 import { SVGIcon } from './SVGIcon';
-
+import './SVGIcon.css';
 
 interface SVGIconThemedProps {
     iconName?: string,
     icon?: string,
+    size?: string,
     lightOpacity?: number,
     darkOpacity?: number,
     width?: string,
@@ -20,6 +21,7 @@ export const SVGIconThemed: React.FC<SVGIconThemedProps> = ({
     icon,
     lightOpacity = 0.4,
     darkOpacity = 0.4,
+    size,
     width,
     height,
     themeMode = 'dark',
@@ -29,22 +31,26 @@ export const SVGIconThemed: React.FC<SVGIconThemedProps> = ({
   const styles: CSSProperties = {
     display: 'flex'
   };
-  
+
   const showedIconName = typeof icon === 'string'
     ? `${themeMode}-${icon}`
     : iconName;
-  
+
   const isLightMode = themeMode === 'light';
-  
+
   const color = isLightMode
     ? `rgba(51, 51, 51, ${lightOpacity})`
     : `rgba(23, 23, 23, ${darkOpacity})`;
   styles.backgroundColor = color;
 
+  const showedClass = `svg-icon-${size} ${className}`;
+
   return (
     <div
       style={styles}
-      onClick={onClick}>
+      onClick={onClick}
+      className={showedClass}
+    >
         <SVGIcon iconName={showedIconName} width={width} height={height} />
     </div>
   );
