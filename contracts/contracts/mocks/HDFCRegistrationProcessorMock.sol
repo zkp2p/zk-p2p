29144098003d64e92ffa0ddb757b5cd1e056b770
bweick <brian.weickmann@gmail.com>
2023-11-27 10:12:30 -0800

    Initial WIP impl of contracts with HDFC/UPI support.

---
+++ contracts/contracts/mocks/HDFCRegistrationProcessorMock.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: MIT
+
+import { IRegistrationProcessor } from "../interfaces/IRegistrationProcessor.sol";
+
+pragma solidity ^0.8.18;
+
+contract HDFCRegistrationProcessorMock is IRegistrationProcessor {
+
+    /* ============ Constructor ============ */
+    constructor() {}
+
+    /* ============ External View Functions ============ */
+    function processProof(
+        RegistrationProof calldata _proof
+    )
+        public
+        pure
+        override
+        returns(bytes32 onRamperIdHash)
+    {
+        return(bytes32(_proof.signals[1]));
+    }
+}
