f04135a9ec9642afc34286c797b0c2e81016cf5a
0xSachinK <sachin@setprotocol.com>
2023-12-05 12:40:12 +0530

    Update gen inputs

--- circuits-circom/scripts/preprocess_input.ts
+++ circuits-circom/scripts/preprocess_input.ts
@@ -6,13 +6,13 @@ export function replaceMessageIdWithXGoogleOriginalMessageId(str) {
 	const messageIdEnd = str.indexOf(">", messageIdStart);
 	const messageId = str.substring(messageIdStart, messageIdEnd);
 
-	console.log("Message ID: ", messageId);
+	console.log("Message ID: ", messageId, "\n");
 
 	const xMessageIdStart = str.indexOf(xGoogleMessageIdLabel) + xGoogleMessageIdLabel.length;
-	const xMessageIdEnd = str.indexOf("\r", xMessageIdStart);
+	const xMessageIdEnd = str.indexOf(".com", xMessageIdStart) + ".com".length;
 	const xMessageId = str.substring(xMessageIdStart, xMessageIdEnd);
 
-	console.log("X-Google-Original-Message-ID: ", xMessageId);
+	console.log("X-Google-Original-Message-ID: ", xMessageId, "\n");
 
 	// Replace "<message-id>" with "x-message-id"
 	return str.replace("<" + messageId + ">", xMessageId);
