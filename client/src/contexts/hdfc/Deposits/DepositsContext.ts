a15581860721f4ea79292d1309e8baae0e96cb1f
Alex Soong <alexsoong91@gmail.com>
2023-12-05 22:34:57 -0800

    Add hdfc deposits context

---
+++ client/src/contexts/hdfc/Deposits/DepositsContext.ts
@@ -0,0 +1,26 @@
+import { createContext } from 'react'
+
+import { DepositWithAvailableLiquidity, DepositIntent } from '../../venmo/Deposits/types'
+
+
+interface DepositsValues {
+  deposits: DepositWithAvailableLiquidity[] | null;
+  depositIntents: DepositIntent[] | null;
+  refetchDeposits: (() => void) | null;
+  shouldFetchDeposits: boolean;
+  refetchDepositIntents: (() => void) | null;
+  shouldFetchDepositIntents: boolean;
+}
+
+const defaultValues: DepositsValues = {
+  deposits: null,
+  depositIntents: null,
+  refetchDeposits: null,
+  shouldFetchDeposits: false,
+  refetchDepositIntents: null,
+  shouldFetchDepositIntents: false
+};
+
+const DepositsContext = createContext<DepositsValues>(defaultValues)
+
+export default DepositsContext
