62c784aec1012339c845016d39c1627c073f3448
Alex Soong <alexsoong91@gmail.com>
2023-12-05 16:16:45 -0800

    Add hdfc registration context

---
+++ client/src/contexts/hdfc/Registration/RegistrationContext.ts
@@ -0,0 +1,20 @@
+import { createContext } from 'react'
+
+
+interface RegistrationValues {
+  isRegistered: boolean;
+  registrationHash: string | null;
+  refetchRampAccount: (() => void) | null;
+  shouldFetchRegistration: boolean;
+}
+
+const defaultValues: RegistrationValues = {
+  isRegistered: false,
+  registrationHash: null,
+  refetchRampAccount: null,
+  shouldFetchRegistration: false
+};
+
+const RegistrationContext = createContext<RegistrationValues>(defaultValues)
+
+export default RegistrationContext
