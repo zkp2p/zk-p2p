de045cd96da0c947ff582868812e24601da6fe76
Richard Liang <richardliang2015@u.northwestern.edu>
2023-12-19 17:45:28 -0500

    Generate venmo send wasm and update script (#218)
    * gen venmo wasm
    
    * update scripts

--- circuits-circom/scripts/7_upload_to_s3.sh
+++ circuits-circom/scripts/7_upload_to_s3.sh
@@ -9,5 +9,8 @@ python3 upload_to_s3.py --dirs ../build/$CIRCUIT_NAME/partial_zkeys/ --upload_di
 echo "Uploading wasm to s3..."
 python3 upload_to_s3.py --dirs ../build/$CIRCUIT_NAME/"$CIRCUIT_NAME"_js/ --upload_dir $UPLOAD_FOLDER/$CIRCUIT_NAME
 
+echo "Uploading c++ dat file to s3"
+python3 upload_to_s3.py --dirs ../build/$CIRCUIT_NAME/"$CIRCUIT_NAME"_cpp/ --upload_dir $UPLOAD_FOLDER/$CIRCUIT_NAME
+
 echo "Uploading zkey and vkey.json to s3..."
 python3 upload_to_s3.py --dirs ../build/$CIRCUIT_NAME/ --upload_dir $UPLOAD_FOLDER/$CIRCUIT_NAME
