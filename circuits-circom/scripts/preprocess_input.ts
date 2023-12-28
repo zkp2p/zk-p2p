1a640e4921f260b12b48a21825f9be38b488f3d0
0xSachinK <sachin@setprotocol.com>
2023-12-28 17:13:21 +0530

    fix preprocess input

--- circuits-circom/scripts/preprocess_input.ts
+++ circuits-circom/scripts/preprocess_input.ts
@@ -1,23 +1,30 @@
 export function hdfcReplaceMessageIdWithXGoogleOriginalMessageId(str) {
 
 	// Replace Message-ID with X-Google-Original-Message-ID if Message-ID contains SMTPIN_ADDED_BROKEN@mx.google.com
-	const messageIdLabel = "Message-ID: <";
+	const messageIdLabel = "Message-ID: ";
 	const xGoogleMessageIdLabel = "X-Google-Original-Message-ID: ";
 
-	const messageIdStart = str.indexOf(messageIdLabel) + messageIdLabel.length;
-	const messageIdEnd = str.indexOf(">", messageIdStart);
-	const messageId = str.substring(messageIdStart, messageIdEnd);
-	console.log("Message ID: ", messageId, "\n");
+	if (str.indexOf(messageIdLabel) == -1) {
+		return str;
+	}
 
-	if (messageId.includes("SMTPIN_ADDED_BROKEN@mx.google.com")) {
-		const xMessageIdStart = str.indexOf(xGoogleMessageIdLabel) + xGoogleMessageIdLabel.length;
-		const xMessageIdEnd = str.indexOf(".com", xMessageIdStart) + ".com".length;
+	const messageIdStart = str.indexOf(messageIdLabel) + messageIdLabel.length;
+	const messageIdEnd = str.indexOf("\n", messageIdStart);
+	const messageId = str.substring(messageIdStart, messageIdEnd);
+
+	console.log("Message ID:", messageId, "\n");
+
+	if (
+		messageId.includes("SMTPIN_ADDED_BROKEN@mx.google.com") ||
+		messageId.includes("SMTPIN_ADDED_BROKEN@MX.GOOGLE.COM")
+	) {
+		const xMessageIdStart = str.indexOf
+			(xGoogleMessageIdLabel) + xGoogleMessageIdLabel.length;
+		const xMessageIdEnd = str.indexOf("\n", xMessageIdStart);
 		const xMessageId = str.substring(xMessageIdStart, xMessageIdEnd);
 
-		console.log("X-Google-Original-Message-ID: ", xMessageId, "\n");
-
-		// Replace "<message-id>" with "x-message-id"
-		return str.replace("<" + messageId + ">", xMessageId);
+		console.log("X-Google-Original-Message-ID:", xMessageId, "\n");
+		return str.replace(messageId, xMessageId);
 	}
 
 	return str;
