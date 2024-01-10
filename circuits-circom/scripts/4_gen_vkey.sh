402c845293d434c3f887f5f57e57371259a84a35
0xSachinK <0xsachink@gmail.com>
2024-01-11 00:11:33 +0530

    Deprecate chunked keygen; Fix scripts

--- circuits-circom/scripts/4_gen_vkey.sh
+++ circuits-circom/scripts/4_gen_vkey.sh
@@ -7,7 +7,7 @@ PHASE1="$PTAU_DIR/powersOfTau28_hez_final_$PTAU.ptau"
 echo "****EXPORTING VKEY****"
 start=$(date +%s)
 set -x
-NODE_OPTIONS='--max-old-space-size=644000' ../node_modules/.bin/snarkjs zkey export verificationkey "$BUILD_DIR"/"$CIRCUIT_NAME".zkey "$BUILD_DIR"/vkey.json
+NODE_OPTIONS='--max-old-space-size=644000' ../circuits/$CIRCUIT_DIR/node_modules/.bin/snarkjs zkey export verificationkey "$BUILD_DIR"/"$CIRCUIT_NAME".zkey "$BUILD_DIR"/vkey.json
 end=$(date +%s)
 { set +x; } 2>/dev/null
 echo "DONE ($((end - start))s)"
--- circuits-circom/scripts/9_gen_sol_calldata.sh
+++ circuits-circom/scripts/9_gen_sol_calldata.sh
@@ -2,4 +2,4 @@
 source circuit.env
 
 echo "Generating solidity calldata..."
-node ../node_modules/.bin/snarkjs zkey export soliditycalldata "$BUILD_DIR"/rapidsnark_public.json "$BUILD_DIR"/rapidsnark_proof.json
+node ../circuits/$CIRCUIT_DIR/node_modules/.bin/snarkjs zkey export soliditycalldata "$BUILD_DIR"/rapidsnark_public.json "$BUILD_DIR"/rapidsnark_proof.json
