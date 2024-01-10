5979e02cc33005304b40276e3063845a185c7350
0xSachinK <0xsachink@gmail.com>
2024-01-11 00:57:32 +0530

    Fix gen verifier script

--- circuits-circom/scripts/8_gen_sol_verifier.sh
+++ circuits-circom/scripts/8_gen_sol_verifier.sh
@@ -2,4 +2,4 @@
 source circuit.env
 
 echo "Generating solidity verifier..."
-node ../circuits/$CIRCUIT_DIR/node_modules/.bin/snarkjs zkey export solidityverifier "$BUILD_DIR"/"$CIRCUIT_NAME".zkey ../contracts/"$CIRCUIT_NAME"_verifier.sol
+node ../circuits/$CIRCUIT_DIR/node_modules/.bin/snarkjs zkey export solidityverifier "$BUILD_DIR"/"$CIRCUIT_NAME".zkey ../circuits/$CIRCUIT_DIR/contracts/"$CIRCUIT_NAME"_verifier.sol
