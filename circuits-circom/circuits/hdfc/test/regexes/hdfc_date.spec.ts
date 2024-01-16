7293bce5633b5bda8aa379451efe1860a7c5081e
Sachin <0xsachink@gmail.com>
2024-01-16 21:45:25 +0300

    Fix HDFC date regex (#276)
    * Fix hdfc date regex
    
    * Add tests
    
    * recompiile (#278)
    
    * recompile and gen verifier
    
    ---------
    
    Co-authored-by: Richard Liang <richardliang2015@u.northwestern.edu>

--- circuits-circom/circuits/hdfc/test/regexes/hdfc_date.spec.ts
+++ circuits-circom/circuits/hdfc/test/regexes/hdfc_date.spec.ts
@@ -35,7 +35,7 @@ describe("HDFC date", function () {
     it("Should generate witnesses", async () => {
         // No commas in amount.
         const input = {
-            "msg": textToAsciiArray("\r\ndate:Sat, 14 Oct 2023 22:09:12 +0530\r\n")
+            "msg": textToAsciiArray("\r\ndate:Tue, 16 Jan 2024 13:18:32 +0530\r\n")
         };
         const witness = await cir.calculateWitness(
             input,
@@ -44,43 +44,88 @@ describe("HDFC date", function () {
         assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)));
     });
 
-    it("Should match regex once", async () => {
-        const input = {
-            "msg": textToAsciiArray("\r\ndate:Sat, 14 Oct 2023 22:09:12 +0530\r\n")
-        };
-        const witness = await cir.calculateWitness(
-            input,
-            true
-        );
-        assert(Fr.eq(Fr.e(witness[1]), Fr.e(1)));
+    describe("when date starts with \r\n", () => {
+        it("Should match regex once", async () => {
+            const input = {
+                "msg": textToAsciiArray("\r\ndate:Tue, 16 Jan 2024 13:18:32 +0530\r\n")
+            };
+            const witness = await cir.calculateWitness(
+                input,
+                true
+            );
+            assert(Fr.eq(Fr.e(witness[1]), Fr.e(1)));
+        });
+
+
+        it("Should reveal regex correctly", async () => {
+            const input = {
+                "msg": textToAsciiArray("\r\ndate:Tue, 16 Jan 2024 13:18:32 +0530\r\n")
+            };
+            const witness = await cir.calculateWitness(
+                input,
+                true
+            );
+            const expected = Array(textToAsciiArray("\r\ndate:").length).fill("0")
+                .concat(textToAsciiArray("Tue, 16 Jan 2024 13:18:32 +0530"))
+                .concat(Array(textToAsciiArray("\r\n").length).fill("0"));
+            const result = witness.slice(2, input.msg.length + 2);
+
+            assert.equal(JSON.stringify(result), JSON.stringify(expected), true);
+        });
+
+        it("Should fail to match regex", async () => {
+            const input = {
+                "msg": textToAsciiArray("\r\ndape:Sat, 14 Oct 2023 22:09:12 +0530\r\n")     // replace t with p
+            };
+            const witness = await cir.calculateWitness(
+                input,
+                true
+            );
+
+            assert(Fr.eq(Fr.e(witness[1]), Fr.e(0)));
+        });
     });
 
+    describe("when date does not start with \r\n", () => {
+        it("Should match regex once", async () => {
+            const input = {
+                "msg": textToAsciiArray("date:Tue, 16 Jan 2024 13:18:32 +0530\r\nto")
+            };
+            const witness = await cir.calculateWitness(
+                input,
+                true
+            );
+            assert(Fr.eq(Fr.e(witness[1]), Fr.e(1)));
+        });
 
-    it("Should reveal regex correctly", async () => {
-        const input = {
-            "msg": textToAsciiArray("\r\ndate:Sat, 14 Oct 2023 22:09:12 +0530\r\n")
-        };
-        const witness = await cir.calculateWitness(
-            input,
-            true
-        );
-        const expected = Array(textToAsciiArray("\r\ndate:").length).fill("0")
-            .concat(textToAsciiArray("Sat, 14 Oct 2023 22:09:12 +0530"))
-            .concat(Array(textToAsciiArray("\r\n").length).fill("0"));
-        const result = witness.slice(2, input.msg.length + 2);
 
-        assert.equal(JSON.stringify(result), JSON.stringify(expected), true);
+        it("Should reveal regex correctly", async () => {
+            const input = {
+                "msg": textToAsciiArray("date:Tue, 16 Jan 2024 13:18:32 +0530\r\nto")
+            };
+            const witness = await cir.calculateWitness(
+                input,
+                true
+            );
+            const expected = Array(textToAsciiArray("date:").length).fill("0")
+                .concat(textToAsciiArray("Tue, 16 Jan 2024 13:18:32 +0530"))
+                .concat(Array(textToAsciiArray("\r\nto").length).fill("0"));
+            const result = witness.slice(2, input.msg.length + 2);
+
+            assert.equal(JSON.stringify(result), JSON.stringify(expected), true);
+        });
+
+        it("Should fail to match regex", async () => {
+            const input = {
+                "msg": textToAsciiArray("dape:Sat, 14 Oct 2023 22:09:12 +0530\r\nto")     // replace t with p
+            };
+            const witness = await cir.calculateWitness(
+                input,
+                true
+            );
+
+            assert(Fr.eq(Fr.e(witness[1]), Fr.e(0)));
+        });
     });
 
-    it("Should fail to match regex", async () => {
-        const input = {
-            "msg": textToAsciiArray("\r\ndape:Sat, 14 Oct 2023 22:09:12 +0530\r\n")     // replace t with p
-        };
-        const witness = await cir.calculateWitness(
-            input,
-            true
-        );
-
-        assert(Fr.eq(Fr.e(witness[1]), Fr.e(0)));
-    });
 });
