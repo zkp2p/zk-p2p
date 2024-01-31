eea88401356ded46e0bc57c0304f9b8294f3a446
Richard Liang <richardliang2015@u.northwestern.edu>
2024-01-31 10:06:22 -0800

    add lifi api

--- client/src/hooks/useLifiBridge.ts
+++ client/src/hooks/useLifiBridge.ts
@@ -1,56 +1,92 @@
-import { LiFi, GetStatusRequest } from '@lifi/sdk';
-
 type LifiRouteRequestParams = {
-  fromAmount: string;
-  fromTokenAddress: string;
-  fromAddress: string;
-  toChainId: number;
-  toTokenAddress: string,
-  toAddress: string
+  toChain: string,
+  fromToken: string,
+  toToken: string,
+  fromAmount: string,
+  fromAddress: string,
+  toAddress: string,
 }
 
 export default function useLifiBridge() {
-    const lifi = new LiFi({
+
+  const getLifiQuotes = async ({fromAmount, fromToken, fromAddress, toChain, toToken, toAddress }: LifiRouteRequestParams) => {
+    const routesRequest = {
+      fromChain: '8453', // Always will be from Base
+      toChain,
+      fromToken, // Fetch USDC from smart contract hook
+      fromAmount,
+      toToken,
+      fromAddress,
+      toAddress,
+      slippage: '0.005', // 0.5% slippage
       integrator: 'ZKP2P',
-      defaultRouteOptions: {
-        integrator: 'ZKP2P',
-        fee: 0,
-        insurance: true,
-        slippage: 0.005,
-        allowDestinationCall: false // Do not allow swapping on destination chain
-      }
+      fee: '0', // 0 fees
+    }
+
+    const apiUrl = 'https://li.quest/v1/quote';
+    const queryParams = new URLSearchParams(routesRequest).toString();
+    const urlWithParams = `${apiUrl}?${queryParams}`;
+
+    const response = await fetch(urlWithParams, {
+      method: 'GET',
     });
 
-    const getLifiRoutes = async ({fromAmount, fromTokenAddress, fromAddress, toChainId, toTokenAddress, toAddress }: LifiRouteRequestParams) => {
-      const routesRequest = {
-        fromChainId: 8453, // Always will be from Base
-        fromAmount,
-        fromTokenAddress,
-        fromAddress,
-        toChainId,
-        toTokenAddress,
-        toAddress,
-      }
-      const result = await lifi.getRoutes(routesRequest);
-      return result.routes;
-    }
+    if (response.ok) {
+      const data = await response.json();
 
-    const getLifiTransactionHistory = async (walletAddress: string, fromTimestamp: number, toTimestamp: number) => {
-      return await lifi.getTransactionHistory({
-        walletAddress,
-        fromTimestamp,
-        toTimestamp
-      });
+      return data;
+    } else {
+      console.error('Failed to fetch quote:', response);
+
+      throw new Error('Failed to fetch quote');
     }
+  }
+
+  const getLifiTransactionHistory = async (walletAddress: string) => {
+    const date = new Date();
+    date.setFullYear(date.getFullYear() - 10);
     
-    const getLifiTransactionStatus = async ({ txHash, bridge, fromChain, toChain }: GetStatusRequest) => {
-      return await lifi.getStatus({
-        txHash,
-        bridge,
-        fromChain,
-        toChain
-      });
-    }
+    const apiUrl = `https://li.quest/v1/analytics/wallets/${walletAddress}`;
+    const queryParams = new URLSearchParams({
+      fromTimestamp: (date.getTime() / 1000).toString(),
+      toTimestamp: (Date.now() / 1000).toString(),
+      integrator: 'ZKP2P',
+    }).toString();
+    const urlWithParams = `${apiUrl}?${queryParams}`;
 
-  return { getLifiRoutes, getLifiTransactionHistory, getLifiTransactionStatus };
+    const response = await fetch(urlWithParams, {
+      method: 'GET',
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+
+      return data;
+    } else {
+      console.error('Failed to fetch transaction history:', response);
+
+      throw new Error('Failed to fetch transaction history');
+    }
+  }
+  
+  const getLifiTransactionStatus = async (txHash: string) => {
+    const apiUrl = `https://li.quest/v1/status`;
+    const urlWithParams = `${apiUrl}?txHash=${txHash}`;
+
+    const response = await fetch(urlWithParams, {
+      method: 'GET',
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+
+      return data;
+    } else {
+      console.error('Failed to fetch status:', response);
+
+      throw new Error('Failed to fetch status');
+    }
+  }
+
+  return { getLifiQuotes, getLifiTransactionHistory, getLifiTransactionStatus };
 };
