72ebfe80aefca71ab0809483972d446e731256b7
Alex Soong <alexsoong91@gmail.com>
2024-02-09 09:58:04 -0800

    Update version to 0.2.3

--- client/src/helpers/constants.ts
+++ client/src/helpers/constants.ts
@@ -32,7 +32,7 @@ export const MAX_HEADER_PADDED_BYTES = 1024; // NOTE: this must be the same as t
 export const MAX_BODY_PADDED_BYTES = 6400; // NOTE: this must be the same as the arg to sha the remainder number of bytes in the email in main args circom
 
 
-export const CLIENT_VERSION = '0.2.2';
+export const CLIENT_VERSION = '0.2.3';
 
 // circom constants from main.circom / https://zkrepl.dev/?gist=30d21c7a7285b1b14f608325f172417b
 // template RSAGroupSigVerify(n, k, levels) {
