ceb2438d13fec8b95a63778ef9ed17d823b337a0
bweick <brian.weickmann@gmail.com>
2024-02-19 08:40:43 -0800

    Committing small progress.

---
+++ contracts/contracts/ramps/wise/mocks/HDFCSendProcessorMock.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: MIT
+
+import { IHDFCSendProcessor } from "../interfaces/IHDFCSendProcessor.sol";
+
+pragma solidity ^0.8.18;
+
+contract HDFCSendProcessorMock is IHDFCSendProcessor {
+
+    /* ============ Constructor ============ */
+    constructor() {}
+
+    /* ============ External View Functions ============ */
+    function processProof(
+        SendProof calldata _proof
+    )
+        public
+        pure
+        override
+        returns(uint256 amount, uint256 timestamp, bytes32 onRamperIdHash, bytes32 offRamperIdHash, bytes32 intentHash)
+    {
+        return(
+            _proof.signals[0],
+            _proof.signals[1],
+            bytes32(_proof.signals[2]),
+            bytes32(_proof.signals[3]),
+            bytes32(_proof.signals[4])
+        );
+    }
+}
