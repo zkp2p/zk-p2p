c3faf3fe40b6a52f29ab31539510564416542445
bweick <brian.weickmann@gmail.com>
2024-03-04 12:02:06 -0800

    Base staging deployment for Garanti contracts. (#333)

--- contracts/deploy/01_deploy_hdfc_contracts.ts
+++ contracts/deploy/01_deploy_hdfc_contracts.ts
@@ -136,7 +136,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 func.skip = async (hre: HardhatRuntimeEnvironment): Promise<boolean> => {
   const network = hre.network.name;
   if (network != "localhost") {
-    try { getDeployedContractAddress(hre.network.name, "VenmoRampV2") } catch (e) {return false;}
+    try { getDeployedContractAddress(hre.network.name, "HDFCRamp") } catch (e) {return false;}
     return true;
   }
   return false;
