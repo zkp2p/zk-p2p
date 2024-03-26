f1baad79704773af80c6d727a8f24b1c369c1cf4
bweick <brian.weickmann@gmail.com>
2024-03-26 11:28:38 -0700

    Added a missing test.

--- contracts/test/ramps/wise/wiseRamp.spec.ts
+++ contracts/test/ramps/wise/wiseRamp.spec.ts
@@ -32,7 +32,7 @@ const expect = getWaffleExpect();
 
 const blockchain = new Blockchain(ethers.provider);
 
-describe.only("WiseRamp", () => {
+describe("WiseRamp", () => {
   let owner: Account;
   let verifier: Account;
   let offRamper: Account;
