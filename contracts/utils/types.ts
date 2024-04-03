a6965d57c8e1bab07d5b4f376ac7f40df92e6b80
Alex Soong <alexsoong91@gmail.com>
2024-04-02 18:10:43 -0700

    Revert changes to contract types

--- contracts/utils/types.ts
+++ contracts/utils/types.ts
@@ -15,3 +15,51 @@ export enum PaymentProviders {
   Garanti = "garanti",
   Wise = "wise",
 }
+
+export interface TLSParams {
+  verifierSigningKey: Address
+  endpoint: string;
+  host: string;
+}
+
+export interface WiseRegistrationData {
+  endpoint: string;
+  host: string;
+  profileId: string;
+  wiseTagHash: string;
+}
+
+export interface WiseRegistrationProof {
+  public_values: WiseRegistrationData;
+  proof: string;
+}
+
+export interface WiseOffRamperRegistrationData {
+  endpoint: string;
+  host: string;
+  profileId: string;
+  mcAccountId: string;
+}
+
+export interface WiseOffRamperRegistrationProof {
+  public_values: WiseOffRamperRegistrationData;
+  proof: string;
+}
+
+export interface WiseSendData {
+  endpoint: string;
+  host: string;
+  transferId: string;
+  senderId: string;
+  recipientId: string;
+  timestamp: string;
+  currencyId: string;
+  amount: string;
+  status: string;
+  intentHash: BigNumber;
+}
+
+export interface WiseSendProof {
+  public_values: WiseSendData;
+  proof: string;
+}
