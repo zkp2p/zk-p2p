b60a29ebd850ab93747e6a5a83c7074dfdbda2b1
0xSachinK <0xsachink@gmail.com>
2024-04-07 17:01:23 +0530

    Update trouble scanning QR code link

--- client/src/components/Swap/SwapModal.tsx
+++ client/src/components/Swap/SwapModal.tsx
@@ -12,7 +12,7 @@ import { PaymentPlatformType } from '@helpers/types';
 import { ThemedText } from '@theme/text';
 import { colors } from '@theme/colors';
 import usePlatformSettings from "@hooks/usePlatformSettings";
-import { ZKP2P_TG_INDIA_CHAT_LINK, ZKP2P_TG_TURKEY_CHAT_LINK } from "@helpers/docUrls";
+import { ZKP2P_TG_INDIA_CHAT_LINK, ZKP2P_TG_TURKEY_CHAT_LINK, WISE_SEND_PAGE_LINK } from "@helpers/docUrls";
 
 
 interface SwapModalProps {
@@ -87,7 +87,7 @@ export const SwapModal: React.FC<SwapModalProps> = ({
 
       case PaymentPlatform.WISE:
         return {
-          troubleScanningQRCodeLink: ZKP2P_TG_INDIA_CHAT_LINK,
+          troubleScanningQRCodeLink: WISE_SEND_PAGE_LINK,
           paymentPlatformName: 'Wise',
           instructionsText: `Scan and send €${amount} <br />to ${venmoId}`,
         };
--- client/src/helpers/docUrls.ts
+++ client/src/helpers/docUrls.ts
@@ -7,6 +7,7 @@ Doc Links
 export const DOWNLOAD_AND_UPLOAD_EMAIL_INSTRUCTIONS_DOCS_LINK = "https://docs.zkp2p.xyz/zkp2p/user-guides/on-ramping/manual-email-input#download-and-upload-email"
 export const COPY_AND_PASTE_EMAIL_INSTRUCTIONS_DOCS_LINK = "https://docs.zkp2p.xyz/zkp2p/user-guides/on-ramping/manual-email-input#copy-and-paste-email"
 export const ZKP2P_SURVEY_FORM_LINK = "https://forms.gle/NYGNHQmh91TMViSu9"
+export const WISE_SEND_PAGE_LINK = "https://wise.com/send#/contact-beta/existing"
 
 
 /*
