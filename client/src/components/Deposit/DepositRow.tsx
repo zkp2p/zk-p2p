18814cd1d9d7354debf8306ba50e5b9ad4a95cad
bert0RM <rigomacedo0629@gmail.com>
2024-05-04 00:59:34 -0700

    fix padding

--- client/src/components/Deposit/DepositRow.tsx
+++ client/src/components/Deposit/DepositRow.tsx
@@ -35,54 +35,64 @@ export const PositionRow: React.FC<PositionRowProps> = ({
   const isMobile = useMediaQuery() === 'mobile'
 
   return (
-    <Container isMobile={isMobile}>
-      <PositionDetailsContainer>
-        <SummaryLabelsAndIconContainer>
-          {
-            !isMobile &&
-            <SVGIconThemed icon={'usdc'} width={'24'} height={'24'}/>
-          }
-          <SummaryLabelsContainer>
-            <SummaryLabel>
-              <SummaryLabelTitle>Available USDC:&nbsp;</SummaryLabelTitle>
-              <SummaryLabelValue>{depositRemainingLabel}</SummaryLabelValue>
-            </SummaryLabel>
-            
-            <SummaryLabel>
-              <SummaryLabelTitle>Outstanding Orders:&nbsp;</SummaryLabelTitle>
-              <SummaryLabelValue>{intentAmountLabel}</SummaryLabelValue>
-            </SummaryLabel>
+    <Container>
+      <IntentDetailsContainer isMobile={isMobile}>
+        <PositionDetailsContainer>
+          <SummaryLabelsAndIconContainer>
+            {
+              !isMobile &&
+                <SVGIconThemed icon={'usdc'} width={'24'} height={'24'}/>
+            }
+            <SummaryLabelsContainer>
+              <SummaryLabel>
+                <SummaryLabelTitle>Available USDC:&nbsp;</SummaryLabelTitle>
+                <SummaryLabelValue>{depositRemainingLabel}</SummaryLabelValue>
+              </SummaryLabel>
 
-            <PercentageLabel>
-              <Label>Conversion Rate:</Label>
-              <Value>{conversionRate}</Value>
-            </PercentageLabel>
+              <SummaryLabel>
+                <SummaryLabelTitle>Outstanding Orders:&nbsp;</SummaryLabelTitle>
+                <SummaryLabelValue>{intentAmountLabel}</SummaryLabelValue>
+              </SummaryLabel>
 
-            <SummaryLabel>
-              <SummaryLabelTitle>Deposit Amount:&nbsp;</SummaryLabelTitle>
-              <SummaryLabelValue>{originalAmountLabel}</SummaryLabelValue>
-            </SummaryLabel>
-          </SummaryLabelsContainer>
-        </SummaryLabelsAndIconContainer>
-      </PositionDetailsContainer>
+              <PercentageLabel>
+                <Label>Conversion Rate:</Label>
+                <Value>{conversionRate}</Value>
+              </PercentageLabel>
 
-      <ActionsContainer>
-        <AccessoryButton
-          onClick={handleWithdrawClick}
-          height={36}
-          loading={isCancelDepositLoading}
-          title={'Withdraw'}
-          icon={'logout'}/>
-      </ActionsContainer>
+              <SummaryLabel>
+                <SummaryLabelTitle>Deposit Amount:&nbsp;</SummaryLabelTitle>
+                <SummaryLabelValue>{originalAmountLabel}</SummaryLabelValue>
+              </SummaryLabel>
+            </SummaryLabelsContainer>
+          </SummaryLabelsAndIconContainer>
+        </PositionDetailsContainer>
+
+        <ActionsContainer>
+          <AccessoryButton
+            onClick={handleWithdrawClick}
+            height={36}
+            loading={isCancelDepositLoading}
+            title={'Withdraw'}
+            icon={'logout'}/>
+        </ActionsContainer>
+      </IntentDetailsContainer>
     </Container>
   );
 };
 
-const Container = styled.div<{isMobile?: boolean}>`
+const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
+const IntentDetailsContainer = styled.div<{isMobile?: boolean}>`
+  width: 100%; 
   display: flex;
   flex-direction: ${({ isMobile }) => isMobile ? 'column' : 'row'};
   align-items: center;
-  height: 100%;
+  padding: 1.25rem 1.5rem;
+  gap: 1.25rem;
+  line-height: 24px;
 `;
 
 const PositionDetailsContainer = styled.div`
@@ -97,7 +107,6 @@ const ActionsContainer = styled.div`
   flex-direction: row;
   align-items: center;
   justify-content: flex-end;
-  padding-right: 1.5rem;
   flex-grow: 1;
 `;
 
