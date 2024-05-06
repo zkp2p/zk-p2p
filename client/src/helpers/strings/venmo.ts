40420fb63a5aca84fe6716d2911b5671054a3840
0xSachinK <0xsachink@gmail.com>
2024-05-06 10:46:16 +0900

    simplify requirement text

--- client/src/helpers/strings/venmo.ts
+++ client/src/helpers/strings/venmo.ts
@@ -85,7 +85,7 @@ const strings: PlatformStrings = {
     Amount USD to send, which may not match your requested USDC amount, is double checked
   `,
   PAYMENT_REQUIREMENT_STEP_THREE: `
-    Do not add more than one emoji in the payment custom note
+    Do not add emojis in the payment note
   `,
   PAYMENT_REQUIREMENT_STEP_FOUR: `
     'Turn on for purchases' at the payment screen is toggled off
