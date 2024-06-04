4c82afd31f0f78a9577ef19f28a7f22c8a8aa785
Alex Soong <alexsoong91@gmail.com>
2024-06-03 17:24:19 -0700

    Add basic BottomNav component

---
+++ client/src/components/layouts/BottomNav/index.tsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import { useLocation } from "react-router-dom";
+import styled from "styled-components";
+
+import { NavItem } from "../TopNav/NavItem";
+
+
+export const BottomNav: React.FC<{ withoutLinks?: boolean }> = ({ withoutLinks }) => {
+  /*
+   * State
+   */
+
+  const location = useLocation();
+  const [selectedItem, setSelectedItem] = useState<string>('Landing');
+
+  /*
+   * Hooks
+   */
+
+  useEffect(() => {
+    const routeName = location.pathname.split('/')[1];
+    setSelectedItem(routeName || 'Landing');
+  }, [location]);
+
+  return (
+    <Wrapper>
+      <NavItemWrapper>
+        <NavItem selectedItem={selectedItem} setSelectedItem={setSelectedItem} />
+      </NavItemWrapper>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  z-index: 1000;
+  padding-bottom: 1.5rem;
+  position: fixed;
+  bottom: 0;
+`;
+
+const NavItemWrapper = styled.div`
+  background: #0D111C;
+  border-radius: 16px;
+  border: 1px solid #98a1c03d;
+  padding: 20px;
+`;
