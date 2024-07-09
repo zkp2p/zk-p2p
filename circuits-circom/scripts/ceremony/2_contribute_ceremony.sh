ec05545cfeddd5d772672a76a726f64466b02e6a
Richard Liang <richardliang2015@u.northwestern.edu>
2024-07-08 20:39:00 -0700

    update entropy

--- circuits-circom/scripts/ceremony/2_contribute_ceremony.sh
+++ circuits-circom/scripts/ceremony/2_contribute_ceremony.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-# You need to set entropy.env for this to work
 
 source circuit.env
 
 R1CS_FILE="$BUILD_DIR/$CIRCUIT_NAME.r1cs"
 PARTIAL_ZKEYS="$BUILD_DIR"/partial_zkeys
 PHASE1="$PTAU_DIR/powersOfTau28_hez_final_$PTAU.ptau"
-source entropy.env
 
 if [ ! -d "$BUILD_DIR"/partial_zkeys ]; then
     echo "No partial_zkeys directory found. Creating partial_zkeys directory..."
@@ -16,7 +14,7 @@ fi
 echo "****GENERATING ZKEY NONCHUNKED 1****"
 start=$(date +%s)
 set -x
-NODE_OPTIONS='--max-old-space-size=112000' node ../circuits/$CIRCUIT_DIR/node_modules/.bin/snarkjs zkey contribute "$PARTIAL_ZKEYS"/"$CIRCUIT_NAME"_"$PREV_VERSION".zkey "$PARTIAL_ZKEYS"/"$CIRCUIT_NAME"_"$CURR_VERSION".zkey --name="1st Contributor Name" -v -e=$ENTROPY2
+NODE_OPTIONS='--max-old-space-size=112000' node ../circuits/$CIRCUIT_DIR/node_modules/.bin/snarkjs zkey contribute "$PARTIAL_ZKEYS"/"$CIRCUIT_NAME"_"$PREV_VERSION".zkey "$PARTIAL_ZKEYS"/"$CIRCUIT_NAME"_"$CURR_VERSION".zkey --name="1st Contributor Name" -v -e=$ENTROPY
 { set +x; } 2>/dev/null
 end=$(date +%s)
 echo "DONE ($((end - start))s)"
