b6361f8ece1ea34c187a34d3534f151ce3031418
Richard Liang <richardliang2015@u.northwestern.edu>
2024-07-08 17:10:20 -0700

    add contrib script

--- circuits-circom/scripts/ceremony/2_contribute_ceremony.sh
+++ circuits-circom/scripts/ceremony/2_contribute_ceremony.sh
@@ -3,6 +3,7 @@
 
 source circuit.env
 
+R1CS_FILE="$BUILD_DIR/$CIRCUIT_NAME.r1cs"
 PARTIAL_ZKEYS="$BUILD_DIR"/partial_zkeys
 PHASE1="$PTAU_DIR/powersOfTau28_hez_final_$PTAU.ptau"
 source entropy.env
@@ -15,7 +16,7 @@ fi
 echo "****GENERATING ZKEY NONCHUNKED 1****"
 start=$(date +%s)
 set -x
-NODE_OPTIONS='--max-old-space-size=112000' node ../circuits/$CIRCUIT_DIR/node_modules/.bin/snarkjs zkey contribute "$PARTIAL_ZKEYS"/"$CIRCUIT_NAME_WITH_VERSION".zkey "$PARTIAL_ZKEYS"/"$CIRCUIT_NAME_WITH_VERSION".zkey --name="1st Contributor Name" -v -e=$ENTROPY1
+NODE_OPTIONS='--max-old-space-size=112000' node ../circuits/$CIRCUIT_DIR/node_modules/.bin/snarkjs zkey contribute "$PARTIAL_ZKEYS"/"$CIRCUIT_NAME"_"$PREV_VERSION".zkey "$PARTIAL_ZKEYS"/"$CIRCUIT_NAME"_"$CURR_VERSION".zkey --name="1st Contributor Name" -v -e=$ENTROPY2
 { set +x; } 2>/dev/null
 end=$(date +%s)
 echo "DONE ($((end - start))s)"
