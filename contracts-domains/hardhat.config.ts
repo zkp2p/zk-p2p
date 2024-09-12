b3239f770ff466267558501a0e80f2a45b874817
Richard Liang <richardliang2015@u.northwestern.edu>
2024-09-12 16:09:11 -0400

    fix coverage attempt

--- contracts-domains/hardhat.config.ts
+++ contracts-domains/hardhat.config.ts
@@ -32,6 +32,15 @@ const config: HardhatUserConfig = {
           },
         },
       },
+      {
+        version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
     ],
   },
   networks: {
